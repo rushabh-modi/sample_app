4ec6d14d07c8f13d7e64ca8100b0123d0611225c
Rushabh Modi <rushabhmodi2552@gmail.com>
2024-04-08 16:47:42 +0530

    fix session_helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,7 +18,10 @@ module SessionsHelper
   # Returns the current logged-in user (if any).
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
+      user = User.find_by(id: user_id)
+      if user && session[:session_token] == user.session_token
+        @current_user = user
+      end
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
